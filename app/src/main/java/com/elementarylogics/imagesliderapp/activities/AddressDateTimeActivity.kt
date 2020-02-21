16128c56fd0f8008d01680fe7357ccfdfbaba367
falcon ele <falconele123@gmail.com>
2020-02-21 11:13:09 +0500

    fourth

--- app/src/main/java/com/elementarylogics/imagesliderapp/activities/AddressDateTimeActivity.kt
+++ app/src/main/java/com/elementarylogics/imagesliderapp/activities/AddressDateTimeActivity.kt
@@ -6,13 +6,11 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.elementarylogics.imagesliderapp.R
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_address_date_time.*
-
-import android.util.Log
-import android.widget.Toast
 
 
 class AddressDateTimeActivity : AppCompatActivity() {
@@ -40,19 +38,24 @@ class AddressDateTimeActivity : AppCompatActivity() {
 
     lateinit var imgCross: ImageView
     lateinit var btnDone: Button
-    lateinit var dateNumberPicker:NumberPicker
-    val dates= arrayOf("tomorrow","feb 21, 2020","feb 22, 2020")
+    lateinit var dateNumberPicker: NumberPicker
+    val dates = arrayOf(
+        "tomorrow  10:00 AM - 10:00 PM",
+        "feb 21, 2020  10:00 AM - 10:00 PM",
+        "feb 22, 2020  10:00 AM - 10:00 PM"
+    )
+
     fun showBottomSheetDialog() {
         val view = layoutInflater.inflate(R.layout.bottomsheet_date_and_time, null)
         imgCross = view.findViewById(R.id.imgCross)
         btnDone = view.findViewById(R.id.btnDone)
-        dateNumberPicker=view.findViewById(R.id.dateNumberPicker)
-        dateNumberPicker.displayedValues=dates
-        dateNumberPicker.maxValue=2
-        dateNumberPicker.minValue=0
+        dateNumberPicker = view.findViewById(R.id.dateNumberPicker)
+        dateNumberPicker.displayedValues = dates
+        dateNumberPicker.maxValue = 2
+        dateNumberPicker.minValue = 0
         dateNumberPicker.setOnValueChangedListener { numberPicker, i, i1 ->
             val valuePicker1 = dateNumberPicker.value
-            Toast.makeText(applicationContext,dates.get(valuePicker1),Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, dates.get(valuePicker1), Toast.LENGTH_SHORT).show()
 
         }
 
@@ -66,13 +69,7 @@ class AddressDateTimeActivity : AppCompatActivity() {
             dialog.dismiss()
         })
 
-        imgCross.setOnClickListener(View.OnClickListener {
-            dialog.dismiss()
-        })
 
-        imgCross.setOnClickListener(View.OnClickListener {
-            dialog.dismiss()
-        })
         dialog.setContentView(view)
         dialog.show()
     }
